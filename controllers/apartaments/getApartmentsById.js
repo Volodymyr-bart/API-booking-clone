f499c54abbd6ede07d6175826c317a447e69f04f
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-03-28 21:32:29 +0300

    change

--- controllers/apartaments/getApartmentsById.js
+++ controllers/apartaments/getApartmentsById.js
@@ -1,2 +1,8 @@
-const getApartmentsById = async (req, res) => {};
+const { Apartment } = require("../../models/apartment");
+
+const getApartmentsById = async (req, res) => {
+  console.log("here");
+  const result = await Apartment.find({});
+  res.json(result);
+};
 module.exports = getApartmentsById;
