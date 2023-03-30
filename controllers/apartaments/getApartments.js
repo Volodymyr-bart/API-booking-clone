63ef2a7dc0b8de23485e0119b7f3a31d1e22cf39
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-03-30 17:11:15 +0300

    edit getApartmentById

--- controllers/apartaments/getApartments.js
+++ controllers/apartaments/getApartments.js
@@ -1,8 +1,8 @@
 const { Apartment } = require("../../models/apartment");
 
 const getApartments = async (req, res) => {
-  console.log("here");
   const result = await Apartment.find({});
+
   res.json(result);
 };
 module.exports = getApartments;
--- controllers/apartaments/getApartmentsById.js
+++ controllers/apartaments/getApartmentsById.js
@@ -1,8 +1,13 @@
+const { HttpError } = require("../../helpers");
 const { Apartment } = require("../../models/apartment");
 
 const getApartmentsById = async (req, res) => {
-  console.log("here");
-  const result = await Apartment.find({});
-  res.json(result);
+  const { id } = req.params;
+  const apartment = await Apartment.findById(id);
+  if (!apartment) {
+    const error = HttpError(404);
+    throw error;
+  }
+  res.json(apartment);
 };
 module.exports = getApartmentsById;
